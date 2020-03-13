3dbaad0edae991e998704ae8e1981155ba7fde75
audunhov <hammerhovda@gmail.com>
2020-03-13 04:08:10 +0100

    Endret fra pasientId til pasient i resepter

--- src/Pasient.java
+++ src/Pasient.java
@@ -1,2 +1,21 @@
 public class Pasient {
+
+
+    private String navn;
+    private String foedselsnr;
+    private Stabel<Resept> resepter;
+
+    public Pasient(String navn, String foedselsnr) {
+        this.navn = navn;
+        this.foedselsnr = foedselsnr;
+    }
+
+    void leggTilResept(Resept resept) {
+        resepter.leggPaa(resept);
+    }
+
+    Stabel<Resept> hentResepter() {
+        return resepter;
+    }
+
 }

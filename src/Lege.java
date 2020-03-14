0ed6c59f0b42f3c59b47587d914a05891250b75a
audunhov <hammerhovda@gmail.com>
2020-03-14 19:39:11 +0100

    La sjekk om utskrift er ulovlig til egen metode

--- src/Lege.java
+++ src/Lege.java
@@ -14,37 +14,36 @@ public class Lege implements Comparable<Lege> {
     }
 
     public String hentNavn() {
-
         return navn;
     }
 
+    private boolean erUlovlig(Legemiddel legemiddel) {
+        return (!(this instanceof Spesialist) && legemiddel instanceof Narkotisk);
+    }
+
     public HvitResept skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
-        if (!(this instanceof Spesialist) && legemiddel instanceof Narkotisk)
-            throw new UlovligUtskrift(this, legemiddel);
+        if (erUlovlig(legemiddel)) throw new UlovligUtskrift(this, legemiddel);
         HvitResept resept = new HvitResept(legemiddel, this, pasient, reit);
         utskrevedeResepter.leggTil(resept);
         return resept;
     }
 
     public MilitaerResept skrivMilitaerResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
-        if (!(this instanceof Spesialist) && legemiddel instanceof Narkotisk)
-            throw new UlovligUtskrift(this, legemiddel);
+        if (erUlovlig(legemiddel)) throw new UlovligUtskrift(this, legemiddel);
         MilitaerResept resept = new MilitaerResept(legemiddel, this, pasient, reit);
         utskrevedeResepter.leggTil(resept);
         return resept;
     }
 
     public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift {
-        if (!(this instanceof Spesialist) && legemiddel instanceof Narkotisk)
-            throw new UlovligUtskrift(this, legemiddel);
+        if (erUlovlig(legemiddel)) throw new UlovligUtskrift(this, legemiddel);
         PResept resept = new PResept(legemiddel, this, pasient);
         utskrevedeResepter.leggTil(resept);
         return resept;
     }
 
     public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
-        if (!(this instanceof Spesialist) && legemiddel instanceof Narkotisk)
-            throw new UlovligUtskrift(this, legemiddel);
+        if (erUlovlig(legemiddel)) throw new UlovligUtskrift(this, legemiddel);
         BlaaResept resept = new BlaaResept(legemiddel, this, pasient, reit);
         utskrevedeResepter.leggTil(resept);
         return resept;

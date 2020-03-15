da51ce946c0331191bfed91f7396315493d5a527
audunhov <hammerhovda@gmail.com>
2020-03-15 13:11:36 +0100

    Statisk Id i Pasient

--- src/Pasient.java
+++ src/Pasient.java
@@ -2,19 +2,44 @@ public class Pasient {
 
 
     private String navn;
+
+
     private String foedselsnr;
     private Stabel<Resept> resepter;
+    private static int counter;
+    private int id;
 
     public Pasient(String navn, String foedselsnr) {
         this.navn = navn;
         this.foedselsnr = foedselsnr;
+        this.id = counter++;
     }
 
-    void leggTilResept(Resept resept) {
+    public int hentId() {
+        return id;
+    }
+
+    public String hentNavn() {
+        return navn;
+    }
+
+    public void settNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String hentFoedselsnr() {
+        return foedselsnr;
+    }
+
+    public void settFoedselsnr(String foedselsnr) {
+        this.foedselsnr = foedselsnr;
+    }
+
+    public void leggTilResept(Resept resept) {
         resepter.leggPaa(resept);
     }
 
-    Stabel<Resept> hentResepter() {
+    public Stabel<Resept> hentResepter() {
         return resepter;
     }
 
